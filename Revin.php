d6af2f32ff34e005f2d49cdd45e8fc99d4adbc41
rev84 <oniduka_takeshi@yahoo.co.jp>
2014-05-26 01:25:18 +0900

    var_dump_file追加

--- Revin.php
+++ Revin.php
@@ -9,10 +9,23 @@ Class Revin{
 		return $buf;
 	}
 
-
 	// tinyurlで短縮URLを取得
 	public static function GetTinyurl($url){
 		return file_get_contents('http://tinyurl.com/api-create.php?url='.$url);
+	}
+	
+	// var_dumpの内容をファイルに書きだす
+	public static function var_dump_file($var, $filename = './var_dump.txt'){
+		if(file_exists($filename)){
+			unlink($filename);
+		}
+		ob_start();
+		var_dump($var);
+		$out = ob_get_contents();
+		ob_end_clean();
+		file_put_contents($filename, $out, FILE_APPEND);
+
+		return true;
 	}
 }
 ?>
